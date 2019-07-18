6c9ffbecac2855e99b1a8306f4c1fc9dfffc5631
Karale <Jaydeep.Karale@newcorp.com>
2019-07-18 07:06:08 -0400

    Added comment to Serializers.py

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers, viewsets
 from .models import Snippet, LANGUAGE_CHOICES, LEXERS, STYLE_CHOICES
 
+#Add a new comment
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model  = Snippet
